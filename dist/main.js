469614b161d456fb6a3ad8d326c90d0d04a5cde2
Christopher Guy <guycorpindustries@outlook.com>
2021-01-13 14:45:33 -0700

    icon

--- dist/main.js
+++ dist/main.js
@@ -23,7 +23,7 @@ fwdBtn.addEventListener('click', () => {
         rwd = false;
         fwdBtnActive = true;
     } else {
-        anim.classList.remove('playFwd');
+        
         fwd = false;
         fwdBtnActive = false;
     }
@@ -69,5 +69,39 @@ stopBtn.addEventListener('click', () => {
     }
 });
 
-// Image Slider
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#nextBtn');
+const prev = document.querySelector('#prevBtn');
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
 
+const nextSlide = () => {
+    const current = document.querySelector('.current');
+    current.classList.remove('current');
+    if(current.nextElementSibling) {
+        current.nextElementSibling.classList.add('current');
+    } else {
+        slides[0].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+}
+
+const prevSlide = () => {
+    const current = document.querySelector('.current');
+    current.classList.remove('current');
+    if (current.previousElementSibling) {
+        current.previousElementSibling.classList.add('current');
+    } else {
+        slides[slides.length - 1].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+}
+
+next.addEventListener('click', e => {
+    nextSlide();
+});
+
+prev.addEventListener('click', e => {
+    prevSlide();
+});
