2505d491d35dacd5579deddb80d1ff1039ceabe3
Christopher Guy <guycorpindustries@outlook.com>
2021-01-14 14:58:05 -0700

    k

--- dist/main.js
+++ dist/main.js
@@ -142,3 +142,22 @@ function galleryClick(e) {
     setTimeout(() => currentImg.classList.remove('fadeIn'), 500);
     e.target.style.opacity = opacity;
 }
+
+
+// Product Gallery
+
+const productImgs = document.getElementsByClassName('productImg');
+const activeProduct = document.getElementsByClassName('activeProduct');
+
+
+for (var i=0; i < productImgs.length; i++){
+    productImgs[i].addEventListener('mouseover', function () {
+       if (activeProduct.length > 0){
+            activeProduct[0].classList.remove('activeProduct')
+        }
+
+        this.classList.add('activeProduct');
+        document.getElementById('productFeature').src = this.src;
+    })
+}
+
