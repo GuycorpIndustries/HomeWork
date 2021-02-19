9c215870bb48c072b4ca1f5a98018b55af0f1d93
Christopher Guy <guycorpindustries@outlook.com>
2021-02-19 12:07:27 -0700

    j

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
+
 import Header from './js/header.js';
 import * as Buttons from './js/buttons.js'
 
 // Creates Header with JavaScript
 Header();
-=======
-import Header from './js/header.js'
-Header();
->>>>>>> 1d69676495c86207c7f1afd682fcfecc159dbcc7
+Buttons();
